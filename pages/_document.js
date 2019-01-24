fec5ae53c4d376b7a2f02e26ca51ee71b227e5b7
drillprop <drylbartosz@gmail.com>
2019-01-24 22:12:01 +0100

    add font to head, so it's preloaded

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <link
+            href='https://fonts.googleapis.com/css?family=Aleo:400,700|Open+Sans:300,400,700&amp;subset=latin-ext'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
