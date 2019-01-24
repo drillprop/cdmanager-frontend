ce150f552dd16ed9979d2d217c383310d38e7c7d
drillprop <drylbartosz@gmail.com>
2019-01-24 19:04:35 +0100

    add GlobalStyles and inject it inside _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Header from '../components/Header';
+import { GlobalStyle } from '../utils/GlobalStyles';
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <Container>
+        <GlobalStyle />
         <Header />
         <Component {...pageProps} />
       </Container>
