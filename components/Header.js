cc32999e85a43df451b7ab18b77b79a162f70c97
drillprop <drylbartosz@gmail.com>
2019-01-24 18:00:19 +0100

    create components for Header

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+import Logo from './Logo';
+
+const Header = () => (
+  <header>
+    <Logo />
+    <Navigation />
+  </header>
+);
+
+export default Header;
---
+++ components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>About</li>
+      <li>Sign in</li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;
