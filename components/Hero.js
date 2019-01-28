86a24d9e036aab202a2c2f7bdc9e6b90d01c33c3
drillprop <drylbartosz@gmail.com>
2019-01-28 23:49:28 +0100

    begin styling the main page, set position of the image, change fonts, set new colors

--- components/Hero.js
+++ components/Hero.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import { mont, robo } from '../utils/fonts';
+import { black, lightblack } from '../utils/colors';
 
 const HeroContainer = styled.div`
   margin: 0 auto;
+  margin-top: 7rem;
   display: grid;
-  grid-gap: 50px;
+  grid-gap: 60px;
   grid-template-columns: 1fr 1fr;
 `;
 
 const StyledHeroHeader = styled.header`
   h1 {
+    color: ${black};
     margin: 0;
     margin-bottom: 1rem;
     font-family: ${robo};
     letter-spacing: -2px;
     text-transform: uppercase;
-    font-size: 2.4rem;
+    font-size: 2rem;
     font-weight: 700;
-    text-align: right;
   }
   h3 {
+    color: ${lightblack};
     margin: 0;
-    text-align: right;
     font-weight: 400;
     font-size: 1rem;
-    line-height: 2.2;
+    line-height: 2;
   }
 `;
 
@@ -44,7 +46,13 @@ const Hero = () => (
       <img
         src='/static/radio.png'
         alt='radio'
-        style={{ maxWidth: '700px', display: 'block', margin: '0 auto' }}
+        style={{
+          maxWidth: '700px',
+          display: 'block',
+          margin: '0 auto',
+          position: 'relative',
+          top: '-75px'
+        }}
       />
     </div>
   </HeroContainer>
