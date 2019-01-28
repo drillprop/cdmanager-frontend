a974d9d1dda989312324fb8db8ea05f6cf13dcec
drillprop <drylbartosz@gmail.com>
2019-01-28 20:32:44 +0100

    start rework hero section, change mostly fonts and sizes

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import { serif } from '../utils/fonts';
+import { mont, robo } from '../utils/fonts';
+
+const HeroContainer = styled.div`
+  width: 1000px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
 
 const StyledHeroHeader = styled.header`
-  width: 300px;
-  margin: 0 auto;
   h1 {
     margin: 0;
     margin-bottom: 1rem;
-    font-family: ${serif};
-    font-size: 3rem;
-    font-weight: 400;
+    font-family: ${robo};
+    letter-spacing: -2px;
+    text-transform: uppercase;
+    font-size: 2.4rem;
+    font-weight: 700;
     text-align: right;
   }
   h3 {
@@ -18,19 +25,22 @@ const StyledHeroHeader = styled.header`
     text-align: right;
     font-weight: 400;
     font-size: 1rem;
-    line-height: 1.8;
+    line-height: 2.2;
   }
 `;
 
 const Hero = () => (
-  <StyledHeroHeader>
-    <h1>Create a your own music library</h1>
-    <h3>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, distinctio
-      quos sequi laudantium ipsum corporis odit! Omnis iste aliquid libero ab
-      asperiores cum, porro suscipit quaerat, facere delectus odio magni!{' '}
-    </h3>
-  </StyledHeroHeader>
+  <HeroContainer>
+    <StyledHeroHeader>
+      <h1>Create a your own music library</h1>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+        distinctio quos sequi laudantium ipsum corporis odit! Omnis iste aliquid
+        libero ab asperiores cum, porro suscipit quaerat, facere delectus odio
+        magni!{' '}
+      </h3>
+    </StyledHeroHeader>
+  </HeroContainer>
 );
 
 export default Hero;
