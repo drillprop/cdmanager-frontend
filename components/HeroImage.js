92d3e889349c99a5e3f1884f1084c86ae3a26e88
drillprop <drylbartosz@gmail.com>
2019-01-30 18:36:13 +0100

    apply shadow to radio

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -9,9 +9,23 @@ const StyledImg = styled.img`
   top: -75px;
 `;
 
+const Shadow = styled.div`
+  background: radial-gradient(#7777 1%, transparent 60%);
+  width: 100px;
+  height: 30px;
+  border-radius: 100%;
+  position: relative;
+  opacity: 0.5;
+  margin: 0;
+  z-index: -2;
+  left: 200px;
+  bottom: 220px;
+  transform: scale(7);
+`;
 const HeroImage = () => (
   <div>
     <StyledImg src='/static/radio.png' alt='radio' />
+    <Shadow />
   </div>
 );
 
