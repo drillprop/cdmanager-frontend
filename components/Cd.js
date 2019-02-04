db03329eeb7bee1be2ce68b9c5aa6a02dce6d07d
drillprop <drylbartosz@gmail.com>
2019-02-04 19:16:34 +0100

    create Cd component

---
+++ components/Cd.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Cd = () => (
+  <figure>
+    <img src="" alt="" />
+    <figcaption>
+      <h3>Black Sabbath </h3>
+      <h4> Paranoid</h4>
+      <h5>1969</h5>
+    </figcaption>
+  </figure>
+);
+
+export default Cd;
