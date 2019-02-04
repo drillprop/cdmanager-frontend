ac06a08012c8d7a236c4b692813196bdad27057e
drillprop <drylbartosz@gmail.com>
2019-02-04 22:37:42 +0100

    create basic layout for addcd

--- components/AddCd.js
+++ components/AddCd.js
@@ -1,23 +1,102 @@
 import React, { Component } from "react";
 import Cd from "./Cd";
+import styled from "styled-components";
+import { robo, mont } from "../utils/fonts";
+import { black, lightblack } from "../utils/colors";
+
+const StyledMain = styled.div`
+  img {
+    display: block;
+    max-height: 90px;
+    margin: 0 auto;
+  }
+`;
+
+const StyledH1 = styled.h1`
+  text-align: center;
+  font-family: ${robo};
+  font-weight: 800;
+  font-size: 3rem;
+  letter-spacing: -3px;
+  color: ${black};
+  margin-bottom: 1rem;
+  margin-top: 0;
+`;
+const StyledForm = styled.form`
+  margin: 0 auto;
+  width: 700px;
+  input {
+    margin: 0 auto;
+    display: block;
+    width: 500px;
+    height: 50px;
+    outline: none;
+    background: white;
+    border: 1px solid ${lightblack};
+    padding: 0 28px;
+    border-radius: 3px;
+    font-family: ${mont};
+    font-size: 1rem;
+  }
+`;
+
+const StyledH2 = styled.h2`
+  position: relative;
+  text-align: center;
+  margin: 2rem 1rem;
+  color: ${lightblack};
+  font-family: ${robo};
+  font-size: 1.2rem;
+  ::after {
+    position: absolute;
+    margin-left: 1rem;
+    content: "";
+    top: 50%;
+    transform: translateY(-50%);
+    width: 10rem;
+    height: 1px;
+    background: black;
+  }
+  ::before {
+    position: absolute;
+    content: "";
+    margin-left: -11rem;
+    top: 50%;
+    border-radius: 4px;
+    transform: translateY(-50%);
+    width: 10rem;
+    height: 1px;
+    background: black;
+  }
+`;
+
+const CdContainer = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
 
 export default class AddCd extends Component {
   render() {
     return (
-      <div>
-        <h1>add an album</h1>
-        <form>
+      <StyledMain>
+        <StyledH1>add an album</StyledH1>
+        <img src="/static/cdinboxlarge.png" alt="cd in box" />
+        <StyledForm>
           <input type="text" placeholder="search..." />
-        </form>
-        <h2>recently added</h2>
-        <section>
+        </StyledForm>
+        <StyledH2>recently added</StyledH2>
+        <CdContainer>
           <Cd />
           <Cd />
           <Cd />
           <Cd />
           <Cd />
-        </section>
-      </div>
+        </CdContainer>
+      </StyledMain>
     );
   }
 }
+
+// TODO
+// change colors to lighter
+//
