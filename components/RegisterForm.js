e870ef65c2ecd786752be7d0002fd46d614e686f
drillprop <drylbartosz@gmail.com>
2019-02-06 21:39:34 +0100

    add onchange and onsubmit handlers, create functions, set states, add inputs attributes

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -2,27 +2,53 @@ import React, { Component } from "react";
 import Button from "../elements/Button";
 class RegisterForm extends Component {
   state = { name: "", email: "", password: "" };
-
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
   render() {
     return (
       <>
         <h1>Sign up for new account</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">
             <p>Name:</p>
-            <input name="name" id="name" type="text" />
+            <input
+              placeholder="your name"
+              required
+              name="name"
+              id="name"
+              type="text"
+              onChange={this.handleChange}
+            />
           </label>
           <label htmlFor="email">
             <p>Email:</p>
-            <input name="email" id="email" type="email" />
+            <input
+              required
+              placeholder="your email"
+              name="email"
+              id="email"
+              type="email"
+              onChange={this.handleChange}
+            />
           </label>
           <label htmlFor="password">
             <p>Password</p>
             <input
+              required
+              placeholder="your password"
               name="password"
               id="password"
               type="password"
-              autocomplete="new-password"
+              aria-autocomplete={"list"}
+              autoComplete="new-password"
+              onChange={this.handleChange}
             />
           </label>
           <br />
