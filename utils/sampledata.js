2fd144f2674285fbef8fe4097f32ce8e708e11d3
drillprop <drylbartosz@gmail.com>
2019-02-06 22:28:47 +0100

    create a data file to basically trying things

---
+++ utils/sampledata.js
@@ -0,0 +1,12 @@
+export const albums = [
+  { artist: "Black Sabbath", title: "Paranoid", year: 1969, image: "url" },
+  { artist: "Iron Maiden", title: "Killers", year: 1972, image: "url" },
+  { artist: "Led Zeppelin", title: "IV", year: 1972, image: "url" },
+  { artist: "Cream", title: "Disraeli Gears", year: 1968, image: "url" },
+  {
+    artist: "Arthur Brown",
+    title: "Crazy World of the Arthur Brown",
+    year: 1969,
+    image: "url"
+  }
+];
