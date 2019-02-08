30046ef891592f1e45e9ee492abdee876d614fdc
drillprop <drylbartosz@gmail.com>
2019-02-08 13:43:43 +0100

    add basic styling to list, list is now mapped from passed props

--- components/SearchList.js
+++ components/SearchList.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../utils/theme";
 import { useSpring, animated } from "react-spring";
+import Cd from "./Cd";
 
 const List = styled(animated.ul)`
   position: relative;
@@ -12,15 +13,35 @@ const List = styled(animated.ul)`
   border: 1px solid #909090;
   border-radius: 3px;
   box-shadow: ${theme.bs};
+  list-style: none;
+`;
+const Item = styled.li`
+  display: flex;
 `;
 
-const SearchList = () => {
+const ImageWrapper = styled.div`
+  width: 70px;
+  height: 70px;
+  border: 1px solid #909090;
+  border-radius: 3px;
+`;
+
+const SearchList = ({ searchResult }) => {
   const smth = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
     <List style={smth}>
-      <li>smth</li>
-      <li>sdasd</li>
-      <li>sadas</li>
+      {searchResult.map(({ artist, title, year, image }) => {
+        return (
+          <Item>
+            <ImageWrapper>{image}</ImageWrapper>
+            <div>
+              <div>{artist}</div>
+              <div>{title}</div>
+              <div>{year}</div>
+            </div>
+          </Item>
+        );
+      })}
     </List>
   );
 };
