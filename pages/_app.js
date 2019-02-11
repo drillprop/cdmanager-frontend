640454dc3a098c2076aab0c8d1cd41fa1dcfaaea
drillprop <drylbartosz@gmail.com>
2019-02-11 18:08:47 +0100

    wrap with withData, add ApolloProvider

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,25 @@ import App, { Container } from 'next/app';
 import Header from '../components/Header';
 import { GlobalStyle } from '../utils/GlobalStyles';
 import { Wrapper } from '../elements/Wrapper';
+import withData from '../lib/withData';
+import { ApolloProvider } from 'react-apollo';
 
-export default class MyApp extends App {
+class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
     return (
       <Container>
         <GlobalStyle />
-        <Header />
-        <Wrapper>
-          <Component {...pageProps} />
-        </Wrapper>
+        <ApolloProvider client={apollo}>
+          {console.log(apollo)}
+          <Header />
+          <Wrapper>
+            <Component {...pageProps} />
+          </Wrapper>
+        </ApolloProvider>
       </Container>
     );
   }
 }
+
+export default withData(MyApp);
