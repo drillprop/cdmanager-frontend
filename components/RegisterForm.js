44f25967af4dd136b22fcc8a75503ab5c4a607b3
drillprop <drylbartosz@gmail.com>
2019-02-11 17:19:20 +0100

    rewrite component with react hooks

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,62 +1,61 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Button from "../elements/Button";
-class RegisterForm extends Component {
-  state = { name: "", email: "", password: "" };
-  handleChange = e => {
+
+const RegisterForm = () => {
+  const [user, setUser] = useState({ name: "", password: "", email: "" });
+
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
+    setUser({ ...user, [name]: value });
   };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
   };
-  render() {
-    return (
-      <>
-        <h1>Sign up for new account</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">
-            <p>Name:</p>
-            <input
-              placeholder="your name"
-              required
-              name="name"
-              id="name"
-              type="text"
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="email">
-            <p>Email:</p>
-            <input
-              required
-              placeholder="your email"
-              name="email"
-              id="email"
-              type="email"
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="password">
-            <p>Password</p>
-            <input
-              required
-              placeholder="your password"
-              name="password"
-              id="password"
-              type="password"
-              aria-autocomplete={"list"}
-              autoComplete="new-password"
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <Button>Register</Button>
-        </form>
-      </>
-    );
-  }
-}
+
+  return (
+    <>
+      <h1>Sign up for new account</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          <p>Name:</p>
+          <input
+            placeholder="your name"
+            required
+            name="name"
+            id="name"
+            type="text"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          <p>Email:</p>
+          <input
+            required
+            placeholder="your email"
+            name="email"
+            id="email"
+            type="email"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          <p>Password</p>
+          <input
+            required
+            placeholder="your password"
+            name="password"
+            id="password"
+            type="password"
+            aria-autocomplete={"list"}
+            autoComplete="new-password"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <Button>Register</Button>
+      </form>
+    </>
+  );
+};
 
 export default RegisterForm;
