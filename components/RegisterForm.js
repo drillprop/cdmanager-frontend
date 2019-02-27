1d8a6b54f5ff081b9bffb7abca244c38fadbedf8
drillprop <drylbartosz@gmail.com>
2019-02-27 22:56:28 +0100

    create graphql mutation and apply it to component

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,59 +1,80 @@
-import React, { Component, useState } from "react";
-import Button from "../elements/Button";
+import gql from 'graphql-tag';
+import React, { useState } from 'react';
+import { Mutation } from 'react-apollo';
+import Button from '../elements/Button';
+
+const SIGNUP = gql`
+  mutation SIGNUP($name: String!, $email: String!, $password: String!) {
+    signup(name: $name, email: $email, password: $password) {
+      name
+      email
+      id
+    }
+  }
+`;
 
 const RegisterForm = () => {
-  const [user, setUser] = useState({ name: "", password: "", email: "" });
+  const [user, setUser] = useState({ name: '', password: '', email: '' });
 
   const handleChange = e => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
-  const handleSubmit = e => {
-    e.preventDefault();
-  };
 
   return (
     <>
-      <h1>Sign up for new account</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          <p>Name:</p>
-          <input
-            placeholder="your name"
-            required
-            name="name"
-            id="name"
-            type="text"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="email">
-          <p>Email:</p>
-          <input
-            required
-            placeholder="your email"
-            name="email"
-            id="email"
-            type="email"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="password">
-          <p>Password</p>
-          <input
-            required
-            placeholder="your password"
-            name="password"
-            id="password"
-            type="password"
-            aria-autocomplete={"list"}
-            autoComplete="new-password"
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <Button>Register</Button>
-      </form>
+      <Mutation mutation={SIGNUP} variables={user}>
+        {(signup, { error, loading }) => (
+          <>
+            <h1>Sign up for new account</h1>
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
+                await signup();
+              }}
+              method='post'
+            >
+              <label htmlFor='name'>
+                <p>Name:</p>
+                <input
+                  placeholder='your name'
+                  required
+                  name='name'
+                  id='name'
+                  type='text'
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor='email'>
+                <p>Email:</p>
+                <input
+                  required
+                  placeholder='your email'
+                  name='email'
+                  id='email'
+                  type='email'
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor='password'>
+                <p>Password</p>
+                <input
+                  required
+                  placeholder='your password'
+                  name='password'
+                  id='password'
+                  type='password'
+                  aria-autocomplete={'list'}
+                  autoComplete='new-password'
+                  onChange={handleChange}
+                />
+              </label>
+              <br />
+              <Button>Register</Button>
+            </form>
+          </>
+        )}
+      </Mutation>
     </>
   );
 };
