0310cfe4519166c32fef480632467ed3cb8d0e9c
drillprop <drylbartosz@gmail.com>
2019-03-07 22:33:13 +0100

    add CdShape

---
+++ elements/CdShape.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Shape = styled.div`
+  position: absolute;
+  left: calc(50% - 20px);
+  width: 470px;
+  height: 470px;
+  border: 1px solid #333;
+  background: white;
+  box-shadow: inset 0 0 0 10px white, inset 0 0 0 165px #333,
+    inset 0 0 0 175px white, inset 0 0 0 176px #333, inset 0 0 0 205px white,
+    inset 0 0 0 206px black;
+  border-radius: 50%;
+  :hover {
+    transition: all 5000ms;
+    transform: rotate(1000deg);
+  }
+`;
+const Text = styled.span`
+  position: relative;
+  top: 45%;
+  left: 70%;
+  color: white;
+`;
+
+const CdShape = () => (
+  <Shape>
+    {' '}
+    <Text>hello</Text>{' '}
+  </Shape>
+);
+
+export default CdShape;
