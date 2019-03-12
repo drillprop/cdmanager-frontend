85ffa89952c4d68a37f82796c44324212539ea93
drillprop <drylbartosz@gmail.com>
2019-03-12 14:03:36 +0100

    add nprogress.css file

--- components/Meta.js
+++ components/Meta.js
@@ -9,6 +9,7 @@ const Meta = () => (
     <meta charSet='UTF-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+    <link rel='stylesheet' href='static/nprogress.css' />
     <title>cd manager</title>
   </Head>
 );
