9e9daa7eaa4af63fb1e722dc3c4552a88545e11c
drillprop <drylbartosz@gmail.com>
2019-03-12 13:51:30 +0100

    create separate file for head

---
+++ components/Meta.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <link
+      href='https://fonts.googleapis.com/css?family=Montserrat:400,500,700|Roboto:400,700,900'
+      rel='stylesheet'
+    />
+    <meta charSet='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+    <title>cd manager</title>
+  </Head>
+);
+
+export default Meta;
