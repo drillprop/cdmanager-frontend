b30399df75ce4760a081cf0cde732a1e760fe73a
drillprop <drylbartosz@gmail.com>
2019-03-15 20:01:18 +0100

    add update prop to Mutation component to refetch query(prop refetchQueries for some reason doesnt refetch every time)

--- components/DeleteButton.js
+++ components/DeleteButton.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
+import { SHOW_RECENTLY_ADDED } from './RecentAlbums';
 
 const DELETE_ALBUM = gql`
   mutation DELETE_ALBUM($id: String!) {
@@ -11,10 +11,38 @@ const DELETE_ALBUM = gql`
   }
 `;
 
-const DeleteButton = ({ id }) => (
-  <Mutation mutation={DELETE_ALBUM} variables={{ id }}>
-    {deleteAlbum => <button onClick={deleteAlbum}>Delete</button>}
-  </Mutation>
-);
+const DeleteButton = ({ id }) => {
+  return (
+    <Mutation
+      mutation={DELETE_ALBUM}
+      variables={{ id }}
+      update={(cache, data) => {
+        const query = {
+          query: SHOW_RECENTLY_ADDED,
+          variables: { last: 20 }
+        };
+        const { albums } = cache.readQuery(query);
+        const updatedCollection = albums.filter(album => album.id !== id);
+        console.log(updatedCollection);
+        cache.writeQuery({
+          ...query,
+          data: {
+            albums: [...updatedCollection]
+          }
+        });
+      }}
+    >
+      {deleteAlbum => (
+        <button
+          onClick={() => {
+            deleteAlbum();
+          }}
+        >
+          Delete
+        </button>
+      )}
+    </Mutation>
+  );
+};
 
 export default DeleteButton;
