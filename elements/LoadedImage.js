70709a4843f18e61543b76276bf6b35578289f7f
drillprop <drylbartosz@gmail.com>
2019-03-15 12:10:18 +0100

    add spring animation to images, replace loading text with Loading component

--- elements/LoadedImage.js
+++ elements/LoadedImage.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
-
+import { useSpring, animated } from 'react-spring';
+import Loading from './Loading';
 // Shows only loaded image
 const LoadedImage = ({ title, image }) => {
   const [imageLoaded, setAsLoaded] = useState(false);
+  const props = useSpring({ opacity: imageLoaded ? 1 : 0 });
+
   return (
     <>
-      <img
-        onLoad={() => setAsLoaded(true)}
-        src={image}
-        alt={title}
-        style={imageLoaded ? {} : { display: 'none' }}
-      />
-      {!imageLoaded && 'Loading...'}
+      <animated.div style={props}>
+        <>
+          <img
+            onLoad={() => setAsLoaded(true)}
+            src={image}
+            alt={title}
+            style={imageLoaded ? { ...props } : { display: 'none', ...props }}
+          />
+        </>
+      </animated.div>
+      {!imageLoaded && <Loading loading={!imageLoaded} />}
     </>
   );
 };
