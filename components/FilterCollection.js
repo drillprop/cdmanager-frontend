3c036f8c34393214521f1f6747b6ce705311cd3a
drillprop <drylbartosz@gmail.com>
2019-03-31 20:28:22 +0200

    add graphql query

--- components/FilterCollection.js
+++ components/FilterCollection.js
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
 import { Input } from '../elements/Form';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const FILTER_ALBUMS = gql`
+  query FILTER_ALBUMS($search: String!) {
+    albumsCollection(search: $search) {
+      title
+      artist
+      image
+      id
+    }
+  }
+`;
 
 const FilterCollection = () => {
   const [value, setValue] = useState('');
@@ -11,6 +24,18 @@ const FilterCollection = () => {
     <>
       <p>Show results for: {value}</p>
       <Input type='text' placeholder='filter' onChange={filter} />{' '}
+      {value && (
+        <Query query={FILTER_ALBUMS} variables={{ search: value }}>
+          {({ data, error, loading }) => {
+            if (error) return <p>{error.message}</p>;
+            if (loading) return <p>Loading...</p>;
+            if (data) {
+              console.log(data);
+              return null;
+            }
+          }}
+        </Query>
+      )}
     </>
   );
 };
