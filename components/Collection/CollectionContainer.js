ced95a9f917e99d760d0cc7febc9445adf230421
drillprop <drylbartosz@gmail.com>
2019-04-10 21:19:40 +0200

    add title at the top of component

--- components/Collection/CollectionContainer.js
+++ components/Collection/CollectionContainer.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import FilterCollection from "./FilterCollection";
 import Pagination from "./Pagination";
 import Albums from "./Albums";
+import { PageTitle } from "../../elements/Titles";
 
 const CollectionContainer = () => {
   const [recentAlbumsVisible, showRecentAlbums] = useState(true);
   return (
     <>
+      <PageTitle>collection</PageTitle>
       <Pagination />
       <FilterCollection showRecentAlbums={showRecentAlbums} />
       {recentAlbumsVisible && <Albums />}
