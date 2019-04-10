6fc055b15ee4a95c16c7d2d6a77c176046956c93
drillprop <drylbartosz@gmail.com>
2019-04-10 18:44:48 +0200

    add new collection page

---
+++ pages/collection.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+import CollectionContainer from "../components/Collection/CollectionContainer";
+
+export const QueryContext = createContext();
+const Collection = ({ query }) => {
+  return (
+    <QueryContext.Provider value={query.page}>
+      <CollectionContainer />
+    </QueryContext.Provider>
+  );
+};
+
+export default Collection;
