d9d47d03bb6afb833a4461e2c53e680b74b4a7d8
drillprop <drylbartosz@gmail.com>
2019-04-10 21:12:48 +0200

    move StyledH1 to elements and rename to PageTitle

---
+++ elements/PageTitle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PageTitle = styled.h1`
+  text-align: center;
+  font-family: ${robo};
+  font-weight: 900;
+  font-size: 3rem;
+  letter-spacing: -1px;
+  color: ${lightblack};
+  margin: 0;
+  margin-bottom: 2rem;
+`;
