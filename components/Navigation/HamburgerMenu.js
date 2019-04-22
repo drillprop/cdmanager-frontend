31eb247e289a8b8b587cbeeafc8ff9555bcb50c8
drillprop <drylbartosz@gmail.com>
2019-04-22 22:38:20 +0200

    start creating curtain menu for mobile devices

--- components/Navigation/HamburgerMenu.js
+++ components/Navigation/HamburgerMenu.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import NavigationLinks from './NavigationLinks';
 
 const StyledDiv = styled.div`
-  cursor: pointer;
   position: fixed;
   height: 25px;
   width: 25px;
@@ -10,7 +10,22 @@ const StyledDiv = styled.div`
   top: 1.5rem;
   right: 0;
   display: none;
+  .toggle {
+    z-index: 10;
+    opacity: 0;
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+    border: none;
+    z-index: -1;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
   div {
+    position: relative;
+    z-index: -2;
+    transition: all 0.5s ease;
     width: 25px;
     height: 3px;
     background-color: red;
@@ -24,13 +39,19 @@ const StyledDiv = styled.div`
       background-color: red;
     }
     ::before {
-      top: 0px;
+      top: -10px;
     }
     ::after {
-      bottom: 0px;
+      top: 10px;
     }
-    :active {
-    }
+  }
+  .toggle:checked + div {
+    transform: rotate(135deg);
+  }
+  .toggle:checked + div:after,
+  .toggle:checked + div:before {
+    top: 0;
+    transform: rotate(90deg);
   }
   @media (max-width: 510px) {
     display: flex;
@@ -39,12 +60,42 @@ const StyledDiv = styled.div`
   }
 `;
 
+const StyledNavigation = styled.nav`
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  z-index: 5;
+  position: fixed;
+  background: black;
+  opacity: 0.9;
+  width: 80%;
+  height: 100%;
+  font-size: 2rem;
+
+  li {
+    list-style: none;
+    text-transform: lowercase;
+  }
+`;
+
 const HamburgerMenu = ({ me, signout }) => {
+  const [toggled, setToggle] = useState(false);
   return (
     <>
       <StyledDiv>
+        <input
+          type='checkbox'
+          onChange={() => setToggle(!toggled)}
+          className='toggle'
+        />
         <div />
       </StyledDiv>
+      {toggled && (
+        <StyledNavigation>
+          <NavigationLinks />
+        </StyledNavigation>
+      )}
     </>
   );
 };
