51e11aa3a0a4a539f5ca32c8564ac0b1c033743c
drillprop <drylbartosz@gmail.com>
2019-04-22 22:33:52 +0200

    resign from passing User props here

--- components/Navigation/NavigationContainer.js
+++ components/Navigation/NavigationContainer.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import User from '../../utils/User';
 import Navigation from './Navigation';
 import HamburgerMenu from './HamburgerMenu';
 
 const NavigationContainer = () => {
   return (
-    <User>
-      {({ me }, signout) => (
-        <>
-          <HamburgerMenu me={me} signout={signout} />
-          <Navigation me={me} signout={signout} />
-        </>
-      )}
-    </User>
+    <>
+      <HamburgerMenu />
+      <Navigation />
+    </>
   );
 };
 
