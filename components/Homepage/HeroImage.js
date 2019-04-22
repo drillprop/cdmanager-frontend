b918bab08f46d96601e863854403d819bc5982ed
drillprop <drylbartosz@gmail.com>
2019-04-22 14:31:50 +0200

    change position from absolute to relative, add max-width to container

--- components/Homepage/HeroImage.js
+++ components/Homepage/HeroImage.js
@@ -4,12 +4,14 @@ import styled from 'styled-components';
 const ImageContainer = styled.div`
   position: relative;
   margin: 0 auto;
+  max-width: 375px;
 `;
 
 const StyledImg = styled.img`
   display: block;
   margin: 0 auto;
   transform: translateY(-40px);
+  position: relative;
 `;
 
 const Shadow = styled.div`
@@ -17,12 +19,12 @@ const Shadow = styled.div`
   width: 700px;
   height: 210px;
   border-radius: 100%;
-  position: absolute;
+  position: relative;
   opacity: 0.5;
   margin: 0;
   z-index: -2;
-  bottom: 50px;
-  left: -150px;
+  bottom: 290px;
+  right: 150px;
 `;
 const HeroImage = () => (
   <ImageContainer>
