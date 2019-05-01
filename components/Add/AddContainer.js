366e173a9eed76c96d7b02b7282748e361d9bba3
drillprop <drylbartosz@gmail.com>
2019-05-01 18:47:20 +0200

    create context in container

--- components/Add/AddContainer.js
+++ components/Add/AddContainer.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useReducer } from 'react';
 import SearchAlbumToAdd from './SearchAlbumToAdd';
 import RecentAlbums from './RecentAlbums';
+import { addReducer } from './addReducer';
+
+const initialState = {
+  searchInput: '',
+  isListVisible: false
+};
+
+export const AddContext = React.createContext({});
 
 const AddContainer = () => {
+  const [state, dispatch] = useReducer(addReducer, initialState);
   const [visible, setToVisible] = useState(true);
   return (
     <>
-      <SearchAlbumToAdd setToVisible={setToVisible} />
-      {visible && <RecentAlbums />}
+      <AddContext.Provider value={{ state, dispatch }}>
+        <SearchAlbumToAdd setToVisible={setToVisible} />
+        {visible && <RecentAlbums />}
+      </AddContext.Provider>
     </>
   );
 };
