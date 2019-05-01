71787c057761d6327505d53ebc9fdd6c05bcaef7
drillprop <drylbartosz@gmail.com>
2019-05-01 18:36:12 +0200

    make up addReducer with correct return

--- components/Add/addReducer.js
+++ components/Add/addReducer.js
@@ -2,7 +2,9 @@ export function addReducer(state, action) {
   switch (action.type) {
     case 'SEARCH_ALBUM':
       return {
-        ...state
+        ...state,
+        searchInput: action.searchInput,
+        isListVisible: !!action.searchInput
       };
     default:
       return state;
