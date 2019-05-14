209d794462dbaf63c1600e1de996ee80fb5b2f85
drillprop <drylbartosz@gmail.com>
2019-05-14 19:38:20 +0200

    change name of variable

--- elements/Albums.js
+++ elements/Albums.js
@@ -1,19 +1,18 @@
-import styled from 'styled-components';
 import React from 'react';
 import Album from './Album';
 import DeleteButton from './DeleteButton';
+import styled from 'styled-components';
 
-const CdContainer = styled.section`
+const CdWrapper = styled.section`
   max-width: 800px;
   margin: 0 auto;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-  grid-gap: 1rem;
 `;
 
 const Albums = ({ albums, deleteButton }) => {
   return (
-    <CdContainer>
+    <CdWrapper>
       {albums
         ? albums.map(({ artist, title, image, id }) => (
             <Album artist={artist} title={title} image={image} key={id} id={id}>
@@ -21,7 +20,7 @@ const Albums = ({ albums, deleteButton }) => {
             </Album>
           ))
         : null}
-    </CdContainer>
+    </CdWrapper>
   );
 };
 export default Albums;
