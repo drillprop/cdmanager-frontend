6188a54a82a320ce2495a24857220494e35a10f6
drillprop <drylbartosz@gmail.com>
2020-03-30 01:46:25 +0200

    remove opacity in HamburgerNavigation, change li color

--- components/Navigation/HamburgerNavigation.js
+++ components/Navigation/HamburgerNavigation.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useTransition, animated } from 'react-spring';
 import User from '../../utils/User';
 import Link from 'next/link';
-import { lighterblack } from '../../utils/colors';
+import { lighterblack, black, background } from '../../utils/colors';
 
 const StyledNavigation = styled(animated.nav)`
   display: none;
@@ -13,12 +13,12 @@ const StyledNavigation = styled(animated.nav)`
   padding: 0;
   z-index: 5;
   position: fixed;
-  background: black;
-  opacity: 0.9;
+  background: ${black};
   width: 80%;
   height: 100%;
   font-size: 2rem;
   li {
+    color: ${background};
     list-style: none;
     position: relative;
     text-transform: lowercase;
@@ -31,7 +31,6 @@ const StyledNavigation = styled(animated.nav)`
       width: 100%;
       height: 1px;
       background-color: ${lighterblack};
-      opacity: 0.2;
     }
   }
   @media (max-width: 600px) {
