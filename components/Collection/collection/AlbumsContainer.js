a7e389069921a61040d02d0c046546e112fabbe9
drillprop <drylbartosz@gmail.com>
2020-04-04 23:01:04 +0200

    add filter input to AlbumsContainer, pass variables to DeleteButton

--- components/Collection/collection/AlbumsContainer.js
+++ components/Collection/collection/AlbumsContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
 import { useCollectionContext } from '../../../contexts/collection/CollectionProvider';
@@ -7,6 +7,17 @@ import { GET_ALBUMS_FROM_COLLECTION } from '../../../utils/queries';
 import Album from '../../Album/Album';
 import DeleteButton from '../../DeleteButton/DeleteButton';
 import Error from '../../Error/Error';
+import debounce from 'lodash.debounce';
+import { Input } from '../../../styles/Form';
+
+const Wrapper = styled.section`
+  max-width: 800px;
+  margin: 0 auto;
+  input {
+    display: block;
+    margin: 0 auto;
+  }
+`;
 
 const StyledH2 = styled.h2`
   text-align: center;
@@ -22,16 +33,37 @@ const CdsWrapper = styled.section`
 `;
 
 const AlbumsContainer = () => {
-  const { page } = useCollectionContext();
+  const { page, state, dispatch } = useCollectionContext();
+  const [result, setValue] = useState('');
+
+  const filter = debounce((text) => {
+    text = text.trim();
+    dispatch({
+      type: 'CHANGE_QUERY_VARIABLES',
+      search: text,
+    });
+    !text && setValue('');
+    return setValue(text);
+  }, 300);
+
   return (
-    <>
-      <StyledH2>recently added</StyledH2>
+    <Wrapper>
+      <Input
+        type='text'
+        placeholder='filter'
+        onChange={(e) => filter(e.target.value)}
+      />
+      <StyledH2>
+        {result ? `searching for ${result}` : 'recently added'}
+      </StyledH2>
       <Query
         query={GET_ALBUMS_FROM_COLLECTION}
-        variables={{ skip: 10 * (page || 1) - 10 }}
-        fetchPolicy='cache-and-network'
+        variables={{
+          ...state.queryVariables,
+          skip: 10 * (page || 1) - 10,
+        }}
       >
-        {({ data, error, loading }) => {
+        {({ data, error, loading, variables }) => {
           if (error) return <Error error={error} />;
           if (loading) return <Loading loading={loading} />;
           if (data?.albums?.albums) {
@@ -46,7 +78,7 @@ const AlbumsContainer = () => {
                     key={id}
                     id={id}
                   >
-                    <DeleteButton id={id} />
+                    <DeleteButton id={id} variables={variables} />
                   </Album>
                 ))}
               </CdsWrapper>
@@ -55,7 +87,7 @@ const AlbumsContainer = () => {
           return <p>no albums added</p>;
         }}
       </Query>
-    </>
+    </Wrapper>
   );
 };
 
