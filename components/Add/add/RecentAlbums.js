4c3742f4488b669a008d72498c65f04c80ea6ba9
drillprop <drylbartosz@gmail.com>
2020-04-04 22:03:25 +0200

    remove Albums component

--- components/Add/add/RecentAlbums.js
+++ components/Add/add/RecentAlbums.js
@@ -3,12 +3,21 @@ import { Query } from 'react-apollo';
 import styled from 'styled-components';
 import Loading from '../../../elements/Loading';
 import { GET_ALBUMS_FROM_COLLECTION } from '../../../utils/queries';
-import Albums from '../../../elements/Albums';
+import Album from '../../Album/Album';
 import Error from '../../Error/Error';
 
 const StyledH2 = styled.h2`
   margin: 2rem 0;
   text-align: center;
+`;
+
+const CdsWrapper = styled.section`
+  max-width: 800px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  justify-content: center;
+  justify-items: center;
 `;
 
 const RecentAlbums = () => {
@@ -24,7 +33,14 @@ const RecentAlbums = () => {
           if (error) return <Error error={error} />;
           if (loading) return <Loading loading={loading} />;
           if (data?.albums?.albums) {
-            return <Albums albums={data.albums.albums} />;
+            const { albums } = data.albums;
+            return (
+              <CdsWrapper>
+                {albums.map(({ artist, title, id, image }) => (
+                  <Album key={id} artist={artist} title={title} image={image} />
+                ))}
+              </CdsWrapper>
+            );
           }
           return <p>no albums added</p>;
         }}
--- components/Collection/collection/AlbumsContainer.js
+++ components/Collection/collection/AlbumsContainer.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
 import { useCollectionContext } from '../../../contexts/collection/CollectionProvider';
-import Albums from '../../../elements/Albums';
 import Loading from '../../../elements/Loading';
 import { GET_ALBUMS_FROM_COLLECTION } from '../../../utils/queries';
+import Album from '../../Album/Album';
+import DeleteButton from '../../DeleteButton/DeleteButton';
 import Error from '../../Error/Error';
 
 const StyledH2 = styled.h2`
   text-align: center;
+`;
+
+const CdsWrapper = styled.section`
+  max-width: 800px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  justify-content: center;
+  justify-items: center;
 `;
 
 const AlbumsContainer = () => {
@@ -25,7 +35,22 @@ const AlbumsContainer = () => {
           if (error) return <Error error={error} />;
           if (loading) return <Loading loading={loading} />;
           if (data?.albums?.albums) {
-            return <Albums albums={data.albums.albums} deleteButton={true} />;
+            const { albums } = data.albums;
+            return (
+              <CdsWrapper>
+                {albums.map(({ artist, title, image, id }) => (
+                  <Album
+                    artist={artist}
+                    title={title}
+                    image={image}
+                    key={id}
+                    id={id}
+                  >
+                    <DeleteButton id={id} />
+                  </Album>
+                ))}
+              </CdsWrapper>
+            );
           }
           return <p>no albums added</p>;
         }}
