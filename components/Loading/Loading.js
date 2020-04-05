8ba1d27ae6029b69f6b9f557593bb1d4ffc1df15
drillprop <drylbartosz@gmail.com>
2020-04-05 18:48:47 +0200

    create loading spinner

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,6 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-const Loading = ({ loading }) => {
+const LoadingSpinner = styled.div`
+  background: white;
+  color: white;
+  font-size: 10px;
+  width: 50px;
+  height: 50px;
+  box-shadow: inset 0 0 0 2px white, inset 0 0 0 17px #333,
+    inset 0 0 0 20px white, inset 0 0 0 21px #333;
+  margin: 14px auto;
+  border: 1px solid #333;
+  border-radius: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${(props) =>
+    !props.isStatic && 'animation: spinner 1000ms linear infinite alternate;'};
+  ::after {
+    content: '';
+    position: absolute;
+    opacity: 0.1;
+    background-color: white;
+    width: 42px;
+    height: 8px;
+    animation: rotate 500ms linear infinite;
+    @keyframes rotate {
+      to {
+        transform: rotate(-360deg);
+      }
+    }
+  }
+  @keyframes spinner {
+    from {
+      transform: translateX(40px);
+    }
+    to {
+      transform: translateX(-40px);
+    }
+  }
+`;
+
+const Loading = ({ loading, withDelay, isStatic }) => {
   const [showLoading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -10,7 +52,10 @@ const Loading = ({ loading }) => {
     return () => {
       clearTimeout(timeout);
     };
-  });
-  return showLoading ? <div>Loading...</div> : null;
+  }, [loading]);
+
+  if (withDelay)
+    return showLoading ? <LoadingSpinner isStatic={isStatic} /> : null;
+  else return <LoadingSpinner isStatic={isStatic} />;
 };
 export default Loading;
