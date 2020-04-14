e0b23cc2cba3c171b706e2877b9f23894799edf7
drillprop <drylbartosz@gmail.com>
2020-04-14 12:32:43 +0200

    show information if there is no album added

--- components/Collection/collection/CollectionAlbums.js
+++ components/Collection/collection/CollectionAlbums.js
@@ -1,14 +1,16 @@
+import debounce from 'lodash.debounce';
+import Link from 'next/link';
 import React, { useState } from 'react';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
 import { useCollectionContext } from '../../../contexts/collection/CollectionProvider';
-import Loading from '../../Loading/Loading';
+import Button from '../../../styles/Button';
+import { Input } from '../../../styles/Form';
 import { GET_ALBUMS_FROM_COLLECTION } from '../../../utils/queries';
 import Album from '../../Album/Album';
-import DeleteButton from './collectionAlbums/DeleteButton';
 import Error from '../../Error/Error';
-import debounce from 'lodash.debounce';
-import { Input } from '../../../styles/Form';
+import Loading from '../../Loading/Loading';
+import DeleteButton from './collectionAlbums/DeleteButton';
 
 const Wrapper = styled.section`
   max-width: 800px;
@@ -34,6 +36,13 @@ const CdsWrapper = styled.div`
   justify-items: center;
 `;
 
+const NoAlbumsPar = styled.p`
+  font-size: 16px;
+  margin-top: 0;
+  margin-bottom: 30px;
+  text-align: center;
+`;
+
 const CollectionAlbums = () => {
   const { page, state, dispatch } = useCollectionContext();
   const [result, setValue] = useState('');
@@ -50,44 +59,58 @@ const CollectionAlbums = () => {
 
   return (
     <Wrapper>
-      <Input
-        type='text'
-        placeholder='filter'
-        onChange={(e) => filter(e.target.value)}
-      />
-      <StyledH2>
-        {result ? `searching for ${result}` : 'recently added'}
-      </StyledH2>
-      <CdsWrapper>
-        <Query
-          query={GET_ALBUMS_FROM_COLLECTION}
-          variables={{
-            ...state.queryVariables,
-            skip: 10 * (page || 1) - 10,
-          }}
-          fetchPolicy='cache-and-network'
-        >
-          {({ data, error, loading, variables }) => {
-            if (error) return <Error error={error} />;
-            if (loading) return <Loading loading={loading} />;
-            if (data?.albums?.albums) {
-              const { albums } = data.albums;
-              return albums.map(({ artist, title, image, id }) => (
-                <Album
-                  artist={artist}
-                  title={title}
-                  image={image}
-                  key={id}
-                  id={id}
-                >
-                  <DeleteButton id={id} variables={variables} />
-                </Album>
-              ));
-            }
-            return <p>no albums added</p>;
-          }}
-        </Query>
-      </CdsWrapper>
+      <Query
+        query={GET_ALBUMS_FROM_COLLECTION}
+        variables={{
+          ...state.queryVariables,
+          skip: 10 * (page || 1) - 10,
+        }}
+        fetchPolicy='cache-and-network'
+      >
+        {({ data, error, loading, variables }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <Loading loading={loading} />;
+          if (data?.albums?.albums) {
+            const { albums } = data.albums;
+            if (!albums.length)
+              return (
+                <>
+                  <NoAlbumsPar>no albums added yet</NoAlbumsPar>
+                  <Button small center>
+                    <Link href={'/add'}>
+                      <a> click here to add one</a>
+                    </Link>
+                  </Button>
+                </>
+              );
+            return (
+              <>
+                <Input
+                  type='text'
+                  placeholder='filter'
+                  onChange={(e) => filter(e.target.value)}
+                />
+                <StyledH2>
+                  {result ? `searching for ${result}` : 'recently added'}
+                </StyledH2>
+                <CdsWrapper>
+                  {albums.map(({ artist, title, image, id }) => (
+                    <Album
+                      artist={artist}
+                      title={title}
+                      image={image}
+                      key={id}
+                      id={id}
+                    >
+                      <DeleteButton id={id} variables={variables} />
+                    </Album>
+                  ))}
+                </CdsWrapper>
+              </>
+            );
+          }
+        }}
+      </Query>
     </Wrapper>
   );
 };
