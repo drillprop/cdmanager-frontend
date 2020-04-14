977250dce87af714b2a9613319a1e5c2b950107e
drillprop <drylbartosz@gmail.com>
2020-04-14 12:31:07 +0200

    add small version of button

--- styles/Button.js
+++ styles/Button.js
@@ -2,14 +2,16 @@ import styled from 'styled-components';
 import { black, background } from '../utils/colors';
 
 const Button = styled.button`
-  border: none;
+  display: block;
+  ${(props) => props.center && 'margin: 0 auto'};
+  border: ${(props) => (props.small ? `1px solid ${black}` : 'none')};
   font-weight: 700;
-  font-size: 1rem;
+  font-size: ${(props) => (props.small ? `0.8rem` : '1rem')};
   padding: 1em 2em;
   border-radius: 5px;
-  text-transform: uppercase;
-  background: ${black || 'black'};
-  color: ${background};
+  text-transform: ${(props) => (props.small ? 'unset' : 'uppercase')};
+  background: ${(props) => (props.small ? background : black)};
+  color: ${(props) => (props.small ? black : background)};
 `;
 
 export default Button;
