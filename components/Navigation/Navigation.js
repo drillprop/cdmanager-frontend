a9b618678012bfda88411a265ab5c9c67aa7ff56
drillprop <drylbartosz@gmail.com>
2020-04-16 18:23:35 +0200

    move StyledNav from Navigation to NavLinks

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -24,54 +24,11 @@ const StyledHeader = styled.header`
   }
 `;
 
-const StyledNav = styled.nav`
-  display: flex;
-  justify-content: flex-end;
-  margin-right: 1rem;
-  ul {
-    display: flex;
-    align-self: center;
-    margin: 0;
-    padding: 0;
-  }
-  li {
-    list-style: none;
-    font-weight: 700;
-    font-style: ${mont};
-    color: ${lightblack};
-    font-size: 0.8rem;
-    text-transform: lowercase;
-    padding: 0.6rem 1.5em;
-    border-radius: 5px;
-    margin-left: 1rem;
-  }
-  li:last-child {
-    position: relative;
-    color: ${background};
-    background-color: ${black};
-    box-shadow: ${theme.bs};
-    ::after {
-      position: absolute;
-      color: ${black};
-      left: 0;
-      top: 3rem;
-      font-weight: 500;
-    }
-  }
-  @media (max-width: 700px) {
-    display: none;
-  }
-`;
-
-const NavItem = styled.li``;
-
 const Navigation = () => {
   return (
     <StyledHeader>
       <Logo />
-      <StyledNav>
-        <NavLinks />
-      </StyledNav>
+      <NavLinks />
       <HamburgerMenu />
     </StyledHeader>
   );
--- components/Navigation/navigation/NavLinks.js
+++ components/Navigation/navigation/NavLinks.js
@@ -1,55 +1,107 @@
 import React from 'react';
 import User from '../../../utils/User';
 import Link from 'next/link';
-import { background, lighterblack } from '../../../utils/colors';
+import {
+  background,
+  lighterblack,
+  lightblack,
+  black,
+} from '../../../utils/colors';
 import Icon from '../../Icon/Icon';
+import styled from 'styled-components';
+import { mont } from '../../../utils/fonts';
+import { theme } from '../../../utils/theme';
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 1rem;
+  ul {
+    display: flex;
+    align-self: center;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    list-style: none;
+    font-weight: 700;
+    font-style: ${mont};
+    color: ${lightblack};
+    font-size: 0.8rem;
+    text-transform: lowercase;
+    padding: 0.6rem 1.5em;
+    border-radius: 5px;
+    margin-left: 1rem;
+  }
+  li a svg {
+    margin-right: 0.5rem;
+  }
+  li:last-child {
+    position: relative;
+    color: ${background};
+    background-color: ${black};
+    box-shadow: ${theme.bs};
+    ::after {
+      position: absolute;
+      color: ${black};
+      left: 0;
+      top: 3rem;
+      font-weight: 500;
+    }
+  }
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
 
 const NavLinks = () => {
   return (
-    <User>
-      {(data, signout) =>
-        data && data.me ? (
-          <ul>
-            <li>
-              <Link href='/login'>
-                <a onClick={signout}>
-                  <Icon icon={'logout'} color={lighterblack} />
-                  logout
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/add'>
-                <a>
-                  <Icon icon={'cd'} color={lighterblack} />
-                  add cd
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/collection'>
-                <a>
-                  <Icon icon={'profile'} color={background} /> your collection
-                </a>
-              </Link>
-            </li>
-          </ul>
-        ) : (
-          <ul>
-            <li>
-              <Link href='/login'>
-                <a>login</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/register'>
-                <a>Register</a>
-              </Link>
-            </li>
-          </ul>
-        )
-      }
-    </User>
+    <StyledNav>
+      <User>
+        {(data, signout) =>
+          data && data.me ? (
+            <ul>
+              <li>
+                <Link href='/login'>
+                  <a onClick={signout}>
+                    <Icon icon={'logout'} color={lighterblack} />
+                    logout
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/add'>
+                  <a>
+                    <Icon icon={'cd'} color={lighterblack} />
+                    add cd
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/collection'>
+                  <a>
+                    <Icon icon={'profile'} color={background} /> your collection
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <Link href='/login'>
+                  <a>login</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/register'>
+                  <a>Register</a>
+                </Link>
+              </li>
+            </ul>
+          )
+        }
+      </User>
+    </StyledNav>
   );
 };
 
