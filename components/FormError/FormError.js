036d35a97f9c110463177247c843414ddaf44f1e
drillprop <drylbartosz@gmail.com>
2020-05-02 21:56:18 +0200

    rename StyledErrorMessage to ErrorList and set padding to 0

--- components/FormError/FormError.js
+++ components/FormError/FormError.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const StyledErrorMessage = styled.ul`
+const ErrorList = styled.ul`
+  padding: 0;
   display: table;
   font-size: 0.8rem;
   margin: 1.5rem auto 0;
@@ -23,9 +24,9 @@ const FormError = ({ error }) => {
 
   return (
     <>
-      <StyledErrorMessage className='error-message'>
+      <ErrorList className='error-message'>
         {errors.length > 0 && errors.map((err) => <li key={err}>{err}</li>)}
-      </StyledErrorMessage>
+      </ErrorList>
     </>
   );
 };
