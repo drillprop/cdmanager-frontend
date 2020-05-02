e5e13c176cc686d9773ab3a36ed1b232f44b8103
drillprop <drylbartosz@gmail.com>
2020-05-02 20:11:24 +0200

    replace Error component with FormError in login and register forms

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -14,7 +14,7 @@ import {
 } from '../../styles/Form';
 import { QUERY_ME } from '../../utils/User';
 import CdShape from '../CdShape/CdShape';
-import Error from '../Error/Error';
+import FormError from '../FormError/FormError';
 
 const LOGIN = gql`
   mutation LOGIN($email: String!, $password: String!) {
@@ -48,61 +48,65 @@ const LoginForm = () => {
         onCompleted={() => redirect({}, '/collection')}
       >
         {(login, { error }) => (
-          <FormWrapper>
-            <Form
-              onSubmit={async (e) => {
-                e.preventDefault();
-                await login();
-                setUser({
-                  name: '',
-                  password: '',
-                  email: '',
-                });
-              }}
-              method='post'
-            >
-              <FormHeader>Login into your account</FormHeader>
-              <Error error={error} />
-              <Label htmlFor='email'>
-                Email
-                <Input
-                  value={user.email}
-                  required
-                  placeholder='your email'
-                  name='email'
-                  id='email'
-                  type='email'
-                  onChange={handleChange}
-                />
-              </Label>
-              <Label htmlFor='password'>
-                Password
-                <Input
-                  value={user.password}
-                  required
-                  placeholder='your password'
-                  name='password'
-                  id='password'
-                  type='password'
-                  aria-autocomplete={'list'}
-                  autoComplete='new-password'
-                  onChange={handleChange}
-                />
-              </Label>
-              <FormButton type='submit'>Login</FormButton>
-              <MobileLinks>
-                Dont have account?
-                <Link href={'/register'}>
-                  <a> Sign up!</a>
-                </Link>
-              </MobileLinks>
-            </Form>
-            <CdShape
-              link='/register'
-              question={`Don't have account?`}
-              answer='Sign up!'
-            />
-          </FormWrapper>
+          <>
+            <FormWrapper>
+              <Form
+                onSubmit={async (e) => {
+                  e.preventDefault();
+                  await login();
+                  setUser({
+                    name: '',
+                    password: '',
+                    email: '',
+                  });
+                }}
+                method='post'
+              >
+                <FormHeader>Login into your account</FormHeader>
+                <FormError error={error} />
+                <Label htmlFor='email'>
+                  Email
+                  <Input
+                    value={user.email}
+                    error={error}
+                    required
+                    placeholder='your email'
+                    name='email'
+                    id='email'
+                    type='email'
+                    onChange={handleChange}
+                  />
+                </Label>
+                <Label htmlFor='password'>
+                  Password
+                  <Input
+                    value={user.password}
+                    error={error}
+                    required
+                    placeholder='your password'
+                    name='password'
+                    id='password'
+                    type='password'
+                    aria-autocomplete={'list'}
+                    autoComplete='new-password'
+                    onChange={handleChange}
+                  />
+                </Label>
+                <FormButton type='submit'>Login</FormButton>
+                <MobileLinks>
+                  Dont have account?
+                  <Link href={'/register'}>
+                    <a> Sign up!</a>
+                  </Link>
+                </MobileLinks>
+              </Form>
+              <CdShape
+                link='/register'
+                question={`Don't have account?`}
+                answer='Sign up!'
+              />
+            </FormWrapper>
+          </>
         )}
       </Mutation>
     </>
--- components/RegisterForm/RegisterForm.js
+++ components/RegisterForm/RegisterForm.js
@@ -14,7 +14,7 @@ import {
 } from '../../styles/Form';
 import { QUERY_ME } from '../../utils/User';
 import CdShape from '../CdShape/CdShape';
-import Error from '../Error/Error';
+import FormError from '../FormError/FormError';
 
 const REGISTER = gql`
   mutation REGISTER($name: String!, $email: String!, $password: String!) {
@@ -38,7 +38,6 @@ const RegisterForm = () => {
     <>
       <Mutation
         mutation={REGISTER}
-        errorPolicy='all'
         variables={user}
         refetchQueries={[
           {
@@ -48,73 +47,75 @@ const RegisterForm = () => {
         onCompleted={() => redirect({}, '/collection')}
       >
         {(register, { error }) => (
-          <FormWrapper>
-            <Form
-              onSubmit={async (e) => {
-                e.preventDefault();
-                await register();
-                setUser({
-                  name: '',
-                  password: '',
-                  email: '',
-                });
-              }}
-              method='post'
-            >
-              <FormHeader>Create account</FormHeader>
-              <Error error={error} />
-              <Label htmlFor='name'>
-                Name
-                <Input
-                  value={user.name}
-                  placeholder='your name'
-                  required
-                  name='name'
-                  id='name'
-                  type='text'
-                  onChange={handleChange}
-                />
-              </Label>
-              <Label htmlFor='email'>
-                Email
-                <Input
-                  value={user.email}
-                  required
-                  placeholder='your email'
-                  name='email'
-                  id='email'
-                  type='email'
-                  onChange={handleChange}
-                />
-              </Label>
-              <Label htmlFor='password'>
-                Password
-                <Input
-                  value={user.password}
-                  required
-                  placeholder='your password'
-                  name='password'
-                  id='password'
-                  type='password'
-                  aria-autocomplete={'list'}
-                  autoComplete='new-password'
-                  onChange={handleChange}
-                />
-              </Label>
-              <FormButton type='submit'>Register</FormButton>
-              <MobileLinks>
-                Already have account?{' '}
-                <Link href={'/login'}>
-                  <a> Sign in!</a>
-                </Link>
-              </MobileLinks>
-            </Form>
-            <CdShape
-              link='/login'
-              question={`Already have account?`}
-              answer='Sign in!'
-            />
-          </FormWrapper>
+          <>
+            <FormWrapper>
+              <Form
+                onSubmit={async (e) => {
+                  e.preventDefault();
+                  await register();
+                  setUser({
+                    name: '',
+                    password: '',
+                    email: '',
+                  });
+                }}
+                method='post'
+              >
+                <FormHeader>Create account</FormHeader>
+                <FormError error={error} />
+                <Label htmlFor='name'>
+                  Name
+                  <Input
+                    value={user.name}
+                    placeholder='your name'
+                    required
+                    name='name'
+                    id='name'
+                    type='text'
+                    onChange={handleChange}
+                  />
+                </Label>
+                <Label htmlFor='email'>
+                  Email
+                  <Input
+                    value={user.email}
+                    required
+                    placeholder='your email'
+                    name='email'
+                    id='email'
+                    type='email'
+                    onChange={handleChange}
+                  />
+                </Label>
+                <Label htmlFor='password'>
+                  Password
+                  <Input
+                    value={user.password}
+                    required
+                    placeholder='your password'
+                    name='password'
+                    id='password'
+                    type='password'
+                    aria-autocomplete={'list'}
+                    autoComplete='new-password'
+                    onChange={handleChange}
+                  />
+                </Label>
+                <FormButton type='submit'>Register</FormButton>
+                <MobileLinks>
+                  Already have account?{' '}
+                  <Link href={'/login'}>
+                    <a> Sign in!</a>
+                  </Link>
+                </MobileLinks>
+              </Form>
+              <CdShape
+                link='/login'
+                question={`Already have account?`}
+                answer='Sign in!'
+              />
+            </FormWrapper>
+          </>
         )}
       </Mutation>
     </>
