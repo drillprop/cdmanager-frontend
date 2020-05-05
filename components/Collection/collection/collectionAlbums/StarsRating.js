618ee2cfc9d44c33bdcc0a60ef06817dea02ad49
drillprop <drylbartosz@gmail.com>
2020-05-05 13:26:17 +0200

    remove for loop from StarsRating, replace Star with Stars

--- components/Collection/collection/collectionAlbums/StarsRating.js
+++ components/Collection/collection/collectionAlbums/StarsRating.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Star from '../../../Star/Star';
 import styled from 'styled-components';
+import Stars from '../../../Stars/Stars';
 
 const StarsRatingWrapper = styled.div`
   position: relative;
@@ -12,39 +12,11 @@ const StarsWrapper = styled.div`
 `;
 
 const StarsRating = () => {
-  const generateStars = () => {
-    const rating = 9 / 2;
-    const stars = [];
-
-    for (let i = 1; i <= 5; i++) {
-      let cover = 0;
-
-      if (i <= rating) {
-        cover = 100;
-      } else {
-        if (i - rating > 1) {
-          cover = 0;
-        } else {
-          cover = (1 - (i - rating)) * 100;
-        }
-      }
-      stars.push(
-        <Star
-          key={i}
-          fillColor='#575757'
-          idx={i}
-          blankColor='silver'
-          size='24px'
-          cover={cover}
-        />
-      );
-    }
-    return stars;
-  };
-
   return (
     <StarsRatingWrapper>
-      <StarsWrapper>{generateStars()}</StarsWrapper>
+      <StarsWrapper>
+        <Stars fillColor='#575757' blankColor='silver' size='24px' cover={50} />
+      </StarsWrapper>
     </StarsRatingWrapper>
   );
 };
