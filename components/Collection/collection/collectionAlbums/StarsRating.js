3311300f862b51aaf761d80b3fe57f849d732b0d
drillprop <drylbartosz@gmail.com>
2020-05-06 16:37:26 +0200

    fill StarsRating with content

--- components/Collection/collection/collectionAlbums/StarsRating.js
+++ components/Collection/collection/collectionAlbums/StarsRating.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 import Stars from '../../../Stars/Stars';
-
-const StarsRatingWrapper = styled.div`
-  position: relative;
-`;
+import { robo } from '../../../../utils/fonts';
 
 const StarsWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  span {
+    :first-of-type {
+      margin: 0 0 7px;
+      font-size: 0.8rem;
+    }
+    margin: 15px 0 5px;
+    font-family: ${robo};
+    font-size: 0.7rem;
+    text-align: center;
+    font-style: italic;
+  }
+  svg {
+    margin-bottom: 15px;
+  }
 `;
 
 const StarsRating = () => {
   return (
-    <StarsRatingWrapper>
-      <StarsWrapper>
-        <Stars fillColor='#575757' blankColor='silver' size='24px' cover={50} />
-      </StarsWrapper>
-    </StarsRatingWrapper>
+    <StarsWrapper>
+      <span>your rating</span>
+      <Stars
+        fillColor='#333'
+        blankColor='silver'
+        size='26px'
+        cover={50}
+        id='your-rating'
+      />
+      <span>users rating</span>
+      <Stars
+        fillColor='#575757'
+        blankColor='silver'
+        size='18px'
+        cover={50}
+        id='user-rating'
+      />
+    </StarsWrapper>
   );
 };
 
