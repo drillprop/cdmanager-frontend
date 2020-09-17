269703af45e440a7967657eab9f8bfd75caab55e
drillprop <drylbartosz@gmail.com>
2020-09-17 13:06:32 +0200

    use apollo hooks in Login Form

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { Mutation } from 'react-apollo';
+import { useMutation } from 'react-apollo';
 import redirect from '../../lib/redirect';
 import {
   Form,
@@ -28,88 +28,81 @@ const LOGIN = gql`
 
 const LoginForm = () => {
   const [user, setUser] = useState({ password: '', email: '' });
+  const [login, { loading, error }] = useMutation(LOGIN, {
+    errorPolicy: 'all',
+    variables: user,
+    refetchQueries: [
+      {
+        query: QUERY_ME,
+      },
+    ],
+    onCompleted: () => redirect({}, '/collection'),
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    await login();
+    setUser({
+      name: '',
+      password: '',
+      email: '',
+    });
+  };
+
   return (
-    <>
-      <Mutation
-        mutation={LOGIN}
-        errorPolicy='all'
-        variables={user}
-        refetchQueries={[
-          {
-            query: QUERY_ME,
-          },
-        ]}
-        onCompleted={() => redirect({}, '/collection')}
-      >
-        {(login, { error }) => (
-          <>
-            <FormWrapper>
-              <Form
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  await login();
-                  setUser({
-                    name: '',
-                    password: '',
-                    email: '',
-                  });
-                }}
-                method='post'
-              >
-                <FormHeader>Login into your account</FormHeader>
-                <FormError error={error} />
-                <Label htmlFor='email'>
-                  Email
-                  <Input
-                    value={user.email}
-                    error={error}
-                    required
-                    placeholder='your email'
-                    name='email'
-                    id='email'
-                    type='email'
-                    onChange={handleChange}
-                  />
-                </Label>
-                <Label htmlFor='password'>
-                  Password
-                  <Input
-                    value={user.password}
-                    error={error}
-                    required
-                    placeholder='your password'
-                    name='password'
-                    id='password'
-                    type='password'
-                    aria-autocomplete={'list'}
-                    autoComplete='new-password'
-                    onChange={handleChange}
-                  />
-                </Label>
-                <FormButton type='submit'>Login</FormButton>
-                <MobileLinks>
-                  Dont have account?
-                  <Link href={'/register'}>
-                    <a> Sign up!</a>
-                  </Link>
-                </MobileLinks>
-              </Form>
-              <CdShape
-                link='/register'
-                question={`Don't have account?`}
-                answer='Sign up!'
-              />
-            </FormWrapper>
-          </>
-        )}
-      </Mutation>
-    </>
+    <FormWrapper>
+      <Form onSubmit={handleLogin} method='post'>
+        <FormHeader>Login into your account</FormHeader>
+        <FormError error={error} />
+        <Label htmlFor='email'>
+          Email
+          <Input
+            value={user.email}
+            error={error}
+            required
+            placeholder='your email'
+            name='email'
+            id='email'
+            type='email'
+            onChange={handleChange}
+          />
+        </Label>
+        <Label htmlFor='password'>
+          Password
+          <Input
+            value={user.password}
+            error={error}
+            required
+            placeholder='your password'
+            name='password'
+            id='password'
+            type='password'
+            aria-autocomplete={'list'}
+            autoComplete='new-password'
+            onChange={handleChange}
+          />
+        </Label>
+        <FormButton type='submit'>
+          {loading ? 'Signing...' : 'Login'}
+        </FormButton>
+        <MobileLinks>
+          Dont have account?
+          <Link href={'/register'}>
+            <a> Sign up!</a>
+          </Link>
+        </MobileLinks>
+      </Form>
+      <CdShape
+        link='/register'
+        question={`Don't have account?`}
+        answer='Sign up!'
+      />
+    </FormWrapper>
   );
 };
 
